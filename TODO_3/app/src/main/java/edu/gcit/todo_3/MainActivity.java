75cd21c0b547b7b46f023bd059256afad8bd7886
unknown <sonamyangchen12190084>
2021-03-23 20:42:43 +0600

    Assignment1

---
+++ TODO_3/app/src/main/java/edu/gcit/todo_3/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.gcit.todo_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void SEND(View view) {
+        Intent intent = new Intent(this,SecondMainActivity2.class);
+        startActivity(intent);
+    }
+}
---
+++ TODO_3/app/src/main/java/edu/gcit/todo_3/SecondMainActivity2.java
@@ -0,0 +1,15 @@
+package edu.gcit.todo_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SecondMainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_main2);
+    }
+}
