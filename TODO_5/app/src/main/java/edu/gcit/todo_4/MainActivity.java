c5ff913389be142c0cb7035e4674c07ba9870a87
unknown <sonamyangchen12190084>
2021-03-23 22:56:29 +0600

    TODO_5

---
+++ TODO_5/app/src/main/java/edu/gcit/todo_4/MainActivity.java
@@ -0,0 +1,89 @@
+package edu.gcit.todo_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG= MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "edu.gcit.todo_4.Messsage";
+    public static final int TEXT_REQUEST = 1;
+    TextView mReplyEditTextHeader;
+    TextView mReplyEditText;
+    EditText mEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "onCreate");
+        Log.d(LOG_TAG,"--------------");
+
+        mReplyEditTextHeader = findViewById(R.id.textView3);
+        mReplyEditText = findViewById(R.id.textView4);
+    }
+
+    public void send(View view) {
+        Log.d(LOG_TAG,"Button click");
+
+        Intent intent= new Intent(this,MainActivity2.class);
+        mEditText = findViewById(R.id.sendEditText);
+        String message = mEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+       super.onActivityResult(requestCode, resultCode, data);
+       if(requestCode == TEXT_REQUEST){
+           if(resultCode == RESULT_OK){
+               String reply = data.getStringExtra(MainActivity2.EXTRA_REPLY);
+               mReplyEditTextHeader.setVisibility(View.VISIBLE);
+               mReplyEditText.setText(reply);
+               mReplyEditText.setVisibility(View.VISIBLE);
+           }
+       }
+    }
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG,"onStart");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(LOG_TAG,"onResume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+}
