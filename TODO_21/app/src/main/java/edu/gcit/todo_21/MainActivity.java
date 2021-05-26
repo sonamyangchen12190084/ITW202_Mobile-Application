ed7331f636cef9617d445d37f5ca4321be30ea1f
unknown <sonamyangchen12190084>
2021-05-26 23:41:14 +0600

    TODO_21

---
+++ TODO_21/app/src/main/java/edu/gcit/todo_21/MainActivity.java
@@ -0,0 +1,25 @@
+package edu.gcit.todo_21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = findViewById(R.id.textView1);
+    }
+
+    public void startTask(View view) {
+        mTextView.setText("napping");
+        new SimpleAsyncTask(mTextView).execute();
+    }
+
+}
